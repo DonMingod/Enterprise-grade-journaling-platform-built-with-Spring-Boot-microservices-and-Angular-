e2dfe82c42db4000fbe0e97a14a36b453a56263c
DonMingod <i202311813@cibertec.edu.pe>
2026-01-20 14:25:19 -0500

    first commit

---
+++ auth-service/src/main/java/com/huisa/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.huisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
